96a03ef39d5b13858801d46f2c1c31afd65fbcef
Jamie Evans <jamieevans@itg.co.uk>
2018-11-09 00:15:11 +0000

    amends

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,6 +16,7 @@ $("ul").on("click", "span", function(e){
 
 $("#down").on("touch click", function (e) { 
     e.preventDefault();
+    alert('CLICKED THE FUCKING BUTTON');
     $("input[type='text'").fadeToggle(300);
 });
 
