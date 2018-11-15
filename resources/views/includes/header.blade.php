3d9f75bfdf8aa0cfdce3fa575a9c92c713a2efff
Jamie Evans <jamieevans@itg.co.uk>
2018-11-15 01:40:12 +0000

    Portfolio Started

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+            <div>
+                <button class="button">LOGO HERE</button>
+                <button class="float-right">Come say Hi!</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+           
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,11 +2,110 @@
 
 @section('content')
     
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>THIS IS AWESOME</h1>
+   
+        <!-- hero section -->
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+            
+                        <div class="top">
+                            <h1>Full-Stack Web Developer & Deployments</h1>
+                            <h5>I do what I love! That just so happens to be web development</h5>
+                        </div>
+
+                        <div class="banner">
+                            <img src="images/banner.jpg" alt="">
+                        </div>
+
+                    </div>
+                </div>
             </div>
-        </div>
+
+        </section>
+        <!-- end hero section -->
+        <!-- intro section -->
+        <section>
+            <div class="container-fluid intro">
+                <div class="container text-center">
+                    <div class="row title">
+                        <div class="col-12 text-center">
+
+                            <div class="title">
+                                <h1>I'm Jamie. This is what I love to do!</h1>
+                                <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio neque accusantium quod esse eligendi eos architecto delectus quaerat maiores velit ab aliquam sunt, qui ad ullam, voluptates distinctio totam iste?</h5>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row table text-center">
+                        <!-- dev col -->
+                        <div class="col-12 col-md-4 sec-col">
+                            <div>
+                                <i class="fa fa-code"></i>
+                                <h3>Develop</h3>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis cupiditate, sit in rerum dolorem enim similique veniam.</p>
+                                <h5>Things I can do!</h5>
+                                <p>HTML, CSS, Javascript, PHP, Node.js </p>
+                                <h5>Tools for the job!</h5>
+                                <ul>
+                                    <li>VSCode</li>
+                                    <li>LESS/SASS</li>
+                                    <li>BitBucket</li>
+                                    <li>Github</li>
+                                    <li>Codepen</li>
+                                    <li>Terminal</li>
+                                    <li>Imagination!</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- deploy col -->
+                        <div class="col-12 col-md-4 sec-col">
+                            <div>
+                                <i class="fa fa-terminal"></i>
+                                <h3>Deploy</h3>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis cupiditate, sit in rerum dolorem enim similique veniam.</p>
+                                <h5>Where do you need it?</h5>
+                                <p>Heroku, git, AWS, GoogleCloud, GoDaddy</p>
+                                <h5>Anything else?</h5>
+                                <ul>
+                                    <li>Install Ubuntu?</li>
+                                    <li>Set up MYSQL?</li>
+                                    <li>Setup PHP?</li>
+                                    <li>Deploy via Git/SSH?</li>
+                                    <li>Dev site for progress checks?</li>
+                                    <li>Deploy to production?</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- enjoy col -->
+                        <div class="col-12 col-md-4">
+                            <div>
+                                <i class="fa fa-coffee"></i>
+                                <h3>Enjoy</h3>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis cupiditate, sit in rerum dolorem enim similique veniam.</p>
+                                <h5>Sit back and relax!</h5>
+                                <p>Everything will get taken care of</p>
+                                <h5>Maintenance you say?</h5>
+                                <ul>
+                                    <li>Updates to exsisting sites</li>
+                                    <li>Amends to current sites</li>
+                                    <li>Amends to exsisting sites</li>
+                                    <li>Re-factoring code</li>
+                                    <li>Re-Designs</li>
+                                    <li>Anything I can do to help</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+       </section>
+       <!-- end intro section -->
     </div>
+
 @endsection
