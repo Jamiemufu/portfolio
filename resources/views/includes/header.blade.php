0b6efc9b3f8095457556b4996ed4d9c7d37aa3a4
Jamie Evans <jamieevans@itg.co.uk>
2018-11-16 13:58:23 +0000

    amended includes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,15 +1,11 @@
-<header>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
+<div class="container">
+    <div class="row">
+        <div class="col-12">
 
-                <div>
-                    <button class="float-right">Come say Hi!</button>
-                </div>
-
+            <div>
+                <button class="float-right">Come say Hi!</button>
             </div>
+
         </div>
     </div>
-</header>
-
-           
+</div>
